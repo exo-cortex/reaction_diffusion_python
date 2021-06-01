c71d926ccb97b709a53722aa2cba5138f0e7fffa
exo-cortex <lukipuki@physik.tu-berlin.de>
2021-06-01 22:19:42 +0200

    Add scaling of input image for flexible grid sizes Add switch of parameter interpolation

--- gray_scott_reaction_diffusion.py
+++ gray_scott_reaction_diffusion.py
@@ -22,24 +22,33 @@ D_A,D_B = Diffusion rates
 
 parser = argparse.ArgumentParser()
 # parser.add_argument("rgb_image", help="set path to u_image")
+parser.add_argument("-multiplicator", default=0, required=False, type=int)
+parser.add_argument("-m", "--method", default=1, required=False, type=int)
 parser.add_argument("-u", "--image_u", help="set path to u_image")
 parser.add_argument("-v", "--image_v", help="set path to v_image")
 # parser.add_argument("-c", "--cropsize", default=[128, 128], type=int, nargs='+', help="Set dimensions of crop", required=True)
 args = parser.parse_args()
 
 save_folder = "./folder"
-save_interval = 500 # save every 50th iteration into image
+save_interval = 128 # save every 50th iteration into image
 if not os.path.exists(save_folder):
-	os.mkdirs(save_folder)
+	os.mkdir(save_folder)
 
 # img_u = Image.open(args.image_u)
 # img_v = Image.open(args.image_v)
 # size = img.size 
 
-dimensions = [256, 256]
+if (abs(args.multiplicator) <= 2):
+	dimensions = [256 * (2 ** args.multiplicator), 256 * (2 ** args.multiplicator)]
+else:
+	dimensions = [512, 512]
+
 
 initial_u = np.array(Image.open(args.image_u).resize((dimensions[1],dimensions[0]),1).convert("L"), dtype=np.float32)/255
 initial_v = np.array(Image.open(args.image_v).resize((dimensions[1],dimensions[0]),1).convert("L"), dtype=np.float32)/255
+
+# initial_u = np.array(Image.open(args.image_u).resize((dimensions[1],dimensions[0]),1).convert("L"), dtype=np.float32)/255
+# initial_v = np.array(Image.open(args.image_v).resize((dimensions[1],dimensions[0]),1).convert("L"), dtype=np.float32)/255
 
 
 initial_u[dimensions[0]//8:dimensions[0]//2,0:dimensions[1]//4] = 0.75
@@ -65,23 +74,31 @@ parameters = {
 
 Du, Dv, F, k = parameters["Worms_1"]
 
-Du = np.zeros(dimensions, dtype=np.float32)
-Dv = np.zeros(dimensions, dtype=np.float32)
 F = np.zeros(dimensions, dtype=np.float32)
 k = np.zeros(dimensions, dtype=np.float32)
 
 a = parameters["Bacteria_1"]
 b = parameters["Coral"]
 
-for i in range(dimensions[0]):
-	for j in range(dimensions[1]):
-		r = np.sqrt((i - 1.5*dimensions[0]/2)**2 + (j - 1.5*dimensions[1]/2)**2) * 2 / (dimensions[0] + dimensions[1])
-		val = 0.5 + 0.5 * np.cos(-80*(r - 0.25)**2)
-		Du[i,j] = (a[0]*val + b[0]*(1-val))
-		Dv[i,j] = (a[1]*val + b[1]*(1-val))
-		F[i,j] = a[2]*val + b[2]*(1-val)
-		k[i,j] = a[3]*val + b[3]*(1-val)        
-		# k[i,j] = val        
+# method = 1
+if args.method == 1:
+	Du = np.zeros(dimensions, dtype=np.float32)
+	Dv = np.zeros(dimensions, dtype=np.float32)
+	for i in range(dimensions[0]):
+		for j in range(dimensions[1]):
+			r = np.sqrt((i - 1.5*dimensions[0]/2)**2 + (j - 1.5*dimensions[1]/2)**2) * 2 / (dimensions[0] + dimensions[1])
+			val = 0.5 + 0.5 * np.cos(-80*(r - 0.25)**2)
+			Du[i,j] = (a[0]*val + b[0]*(1-val))
+			Dv[i,j] = (a[1]*val + b[1]*(1-val))
+			F[i,j] = (a[2]*val + b[2]*(1-val))
+			k[i,j] = (a[3]*val + b[3]*(1-val))
+
+if args.method == 2:
+	for i in range(dimensions[0]):
+		for j in range(dimensions[1]):
+			F[i,j] = 0.018 + (0.06 - 0.016) * i / dimensions[0]
+			k[i,j] = 0.05 + (0.07 - 0.05) * j / dimensions[1]
+
 
 # Image.fromarray(np.uint8(k*255)).save("k_image.png")
 
